69a0ee17a6fec8ca68f300afafad12429d500a07
Kenji Suzuki <41730006+suzulabo@users.noreply.github.com>
2022-12-28 22:40:25 +0900

    Add @announcing/types

---
+++ packages/types/src/index.ts
@@ -0,0 +1,8 @@
+export {
+  AnnouncingJSON,
+  validateAnnouncingJSON,
+} from './ajv/AnnouncingJSON/AnnouncingJSON';
+export {
+  AnnouncingPostsJSON,
+  validateAnnouncingPostsJSON,
+} from './ajv/AnnouncingJSON/AnnouncingPostsJSON';
